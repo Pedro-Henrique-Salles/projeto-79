43dcf26e953f2dfae86d8e1e4de93423b7bba67d
Pedro-Henrique-Salles <110946918+Pedro-Henrique-Salles@users.noreply.github.com>
2023-03-17 17:03:46 -0400

    Add files via upload
    atualizando

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
-enuListArray = ["Pizza Vegetariana",
+menuListArray = ["Pizza Vegetariana",
                  "Pizza deFrango",
                  "Pizza Portuguesa",
-                 "Pizza Quatro Queijos"
-                 "Pizza de Calabresa"
+                 "Pizza Quatro Queijos",
+                 "Pizza de Calabresa",
                 "Pizza Extravaganza"   ];
 
 function getMenu(){
-var htmldata;
+var htmldata="";
 menuListArray.sort();
-for(var i=0;i)<menuListArray.length;i++){
-    Htmldata=htmldata+ menuListArray[i] + '<br>'
+for(var i=0;i<menuListArray.length;i++){
+    htmldata=htmldata+ menuListArray[i] + '<br>';
     }
-    Document.getElementById(“displayMenu”).innerHTML = htmldata
+    document.getElementById("displayMenu").innerHTML = htmldata;
     
 }
 
 function addItem(){
-var htmldata;
-var item=document.getElementById("addItem").value;
+    var imagem="<img src='pizzaImg.png' id='im1'>";
+var htmldata="";
 menuListArray.sort();
-htmldata=””
 for(var i=0;i<menuListArray.length;i++){
-htmldata=htmldata+imgtags+ menuListArray[i]+’<br>’;
+htmldata=htmldata+imagem+ menuListArray[i]+'<br>';
 }
-Document.getElementById(“displayAddeMenu”).innerHtml = htmldata
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
 
 
 }
 
 function addTop(){
-    Var item=document.getElementById(“addItem”).value;
+    var item=document.getElementById("addItem").value;
     menuListArray.push(item);
     addItem();
     
